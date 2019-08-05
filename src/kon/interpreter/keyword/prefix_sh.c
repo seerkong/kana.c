649d1c7425167d841fc845cd22203822ae71b919
kongweixian <kong_weixian@163.com>
2019-08-06 01:13:28 +0800

    add shell cmd marcro

---
+++ src/kon/interpreter/keyword/prefix_sh.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "prefix_if.h"
+#include "../cps_interpreter.h"
+
+KonTrampoline* KON_EvalPrefixSh(KonState* kstate, KN expression, KN env, KonContinuation* cont)
+{
+    KON_DEBUG("meet prefix marcro sh");
+    KON_DEBUG("rest words %s", KON_StringToCstr(KON_ToFormatString(kstate, expression, true, 0, "  ")));
+    
+    KxStringBuffer* sb = KxStringBuffer_New();
+
+    KN iter = expression;
+    while (iter != KON_NIL) {
+        KN next = KON_CDR(iter);
+        KN item = KON_CAR(iter);
+        if (KON_IS_SYMBOL(item)) {
+            KxStringBuffer_AppendCstr(sb, KON_UNBOX_SYMBOL(item));
+        }
+        else if (kon_is_string(item)) {
+            KxStringBuffer_AppendCstr(sb, KxStringBuffer_Cstr(KON_UNBOX_STRING(item)));
+        }
+
+        if (next != KON_NIL) {
+            KxStringBuffer_AppendCstr(sb, " ");
+        }
+        iter = next;
+    }
+
+    KON_DEBUG("exec shell cmd: %s", KxStringBuffer_Cstr(sb));
+    system(KxStringBuffer_Cstr(sb));
+
+    KonTrampoline* bounce;
+    bounce = AllocBounceWithType(KON_TRAMPOLINE_RUN);
+    bounce->Run.Value = KON_ONE;
+    bounce->Run.Cont = cont;
+
+    return bounce;
+}
