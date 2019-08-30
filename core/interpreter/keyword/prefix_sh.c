739bc0ebc8d1d9545a8e7beb387b19325a50ac95
kongweixian <kong_weixian@163.com>
2019-08-30 14:35:04 +0800

    eval shell cmd words

--- core/interpreter/keyword/prefix_sh.c
+++ core/interpreter/keyword/prefix_sh.c
@@ -3,38 +3,92 @@
 #include "prefix_if.h"
 #include "../cps_interpreter.h"
 
+KN CmdWordToIdentifier(KN source)
+{
+    if (KN_IS_WORD(source)) {
+        ((KonSymbol*)source)->Type = KN_SYM_IDENTIFIER;
+    }
+    return source;
+}
+
+KN AfterShellCmdEvaled(KonState* kstate, KN evaledValue, KonContinuation* contBeingInvoked)
+{
+    KonEnv* env = contBeingInvoked->Env;
+    KxHashTable* memo = KxHashTable_ShadowClone(contBeingInvoked->Native.MemoTable);
+    KN restCmdWords = KxHashTable_AtKey(memo, "RestCmdWords");
+    KN evaled = KxHashTable_AtKey(memo, "Evaled");
+    evaled = KN_CONS(kstate, evaledValue, evaled);
+    KN_DEBUG("restCmdWords %s", KN_StringToCstr(KN_ToFormatString(kstate, restCmdWords, true, 0, "  ")));
+    
+    KN_DEBUG("evaled %s", KN_StringToCstr(KN_ToFormatString(kstate, evaled, true, 0, "  ")));
+    
+
+    KonTrampoline* bounce;
+    if (restCmdWords == KN_NIL) {
+        KxStringBuffer* sb = KxStringBuffer_New();
+
+        KN iter = KN_PairListRevert(kstate, evaled);
+        while (iter != KN_NIL) {
+            KN next = KN_CDR(iter);
+            KN item = KN_CAR(iter);
+            if (KN_IS_SYMBOL(item)) {
+                KxStringBuffer_AppendCstr(sb, KN_UNBOX_SYMBOL(item));
+            }
+            else if (KN_IS_STRING(item)) {
+                KxStringBuffer_AppendCstr(sb, KxStringBuffer_Cstr(KN_UNBOX_STRING(item)));
+            }
+
+            if (next != KN_NIL) {
+                KxStringBuffer_AppendCstr(sb, " ");
+            }
+            iter = next;
+        }
+
+        KN_DEBUG("exec shell cmd: %s", KxStringBuffer_Cstr(sb));
+        system(KxStringBuffer_Cstr(sb));
+        KN_DEBUG("exec shell cmd finished");
+
+        bounce = AllocBounceWithType(kstate, KN_TRAMPOLINE_RUN);
+        bounce->Run.Value = KN_TRUE;
+        bounce->Cont = contBeingInvoked->Cont;
+    }
+    else {
+        // next condition
+        KonContinuation* k = AllocContinuationWithType(kstate, KN_CONT_NATIVE_CALLBACK);
+        k->Cont = contBeingInvoked->Cont;
+        k->Env = env;
+
+        KxHashTable* memo = KxHashTable_Init(4);
+        KxHashTable_PutKv(memo, "RestCmdWords", KN_CDR(restCmdWords));
+        KxHashTable_PutKv(memo, "Evaled", evaled);
+
+        k->Native.MemoTable = memo;
+        k->Native.Callback = AfterShellCmdEvaled;
+
+        bounce = KN_EvalExpression(kstate, CmdWordToIdentifier(KN_CAR(restCmdWords)), env, k);
+    }
+
+    return bounce;
+}
+
 KonTrampoline* KN_EvalPrefixSh(KonState* kstate, KN expression, KonEnv* env, KonContinuation* cont)
 {
     KN_DEBUG("meet prefix marcro sh");
-    KN_DEBUG("rest words %s", KN_StringToCstr(KN_ToFormatString(kstate, expression, true, 0, "  ")));
     KN arguments = KN_CellCoresToList(kstate, expression);
+    KN_DEBUG("rest words %s", KN_StringToCstr(KN_ToFormatString(kstate, arguments, true, 0, "  ")));
 
-    KxStringBuffer* sb = KxStringBuffer_New();
+    KonContinuation* k = AllocContinuationWithType(kstate, KN_CONT_NATIVE_CALLBACK);
+    k->Cont = cont;
+    k->Env = env;
 
-    KN iter = arguments;
-    while (iter != KN_NIL) {
-        KN next = KN_CDR(iter);
-        KN item = KN_CAR(iter);
-        if (KN_IS_SYMBOL(item)) {
-            KxStringBuffer_AppendCstr(sb, KN_UNBOX_SYMBOL(item));
-        }
-        else if (KN_IS_STRING(item)) {
-            KxStringBuffer_AppendCstr(sb, KxStringBuffer_Cstr(KN_UNBOX_STRING(item)));
-        }
+    KxHashTable* memo = KxHashTable_Init(4);
+    KxHashTable_PutKv(memo, "RestCmdWords", KN_CDR(arguments));
+    KxHashTable_PutKv(memo, "Evaled", KN_NIL);
 
-        if (next != KN_NIL) {
-            KxStringBuffer_AppendCstr(sb, " ");
-        }
-        iter = next;
-    }
+    k->Native.MemoTable = memo;
+    k->Native.Callback = AfterShellCmdEvaled;
 
-    KN_DEBUG("exec shell cmd: %s", KxStringBuffer_Cstr(sb));
-    system(KxStringBuffer_Cstr(sb));
-
-    KonTrampoline* bounce;
-    bounce = AllocBounceWithType(kstate, KN_TRAMPOLINE_RUN);
-    bounce->Run.Value = KN_TRUE;
-    bounce->Cont = cont;
+    KonTrampoline* bounce = KN_EvalExpression(kstate, CmdWordToIdentifier(KN_CAR(arguments)), env, k);
 
     return bounce;
 }
